1569099470888782c7bd21add52fad3ee3638f39
John Schleider <JS.Tech@comcast.net>
2022-10-03 01:14:52 -0400

    Initial commit of test project

---
+++ .storybook/main.js
@@ -0,0 +1,18 @@
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  staticDirs: ['../public'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+  ],
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-webpack5',
+  },
+  features: {
+    interactionsDebugger: true,
+  },
+};
